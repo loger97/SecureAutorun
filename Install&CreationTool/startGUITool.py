6897f5d4b9d32888ba74a2b899ae58b405bc6166
loger <schirmdexter+github@gmail.com>
2026-08-30 02:08:10 -0500

    Added section for drives

--- Install&CreationTool/startGUITool.py
+++ Install&CreationTool/startGUITool.py
@@ -1,43 +1,44 @@
-from libraries.guizero import *
-from tkinter import Tk, Message
+from guizero import *
+from tkinter import Tk
 from tkinter.filedialog import *
 import os
 import hashlib
+import win32api
 
+nonNTFS = []
 def createDrive():
+    indexNonNTFSVolumes()
     global securityCode, iniFile, batFile
-    driveMaker.visible = True
+    app.visible = True
     def openSelectDriveRoot():
         global drivePath
-        Tk().withdraw()   # we don't want a full GUI, so keep the root window from appearing
-        drivePath = select_folder()  # show an "Open" dialog box and return the path to the selected drive
-        print("Drive: " + drivePath)
-        print(os.listdir(drivePath))
+        drivePath = driveIndex.value
     def selectIcon():
         Tk().withdraw()
         iconFile = askopenfilename(defaultextension=".ico", title="Select Icon - Drive Flash Utility")
         print("Icon: " + iconFile)
-    PushButton(driveMaker, text="Select Drive Root", command=openSelectDriveRoot, grid=[0,0])
+    # PushButton(driveMaker, text="Select Drive Root", command=openSelectDriveRoot, grid=[0,0])
+    driveIndex = Combo(app, options=nonNTFS, grid=[0, 0], command=openSelectDriveRoot)
     # User writes .ini file
-    iniFileTitleBox = TitleBox(driveMaker, "autorun.inf", grid=[0,1])
+    iniFileTitleBox = TitleBox(app, "autorun.inf", grid=[0, 1])
     iniFile = TextBox(iniFileTitleBox, text="[autorun]\n"
                                             "icon = autorun.ico\n"
                                             "label = (Drive Name Here)\n"
                                             "open = start.bat", multiline=True, width=55, height=15)
 
     # User writes start.bat
-    batFileTitleBox = TitleBox(driveMaker, "start.bat", grid=[1,1])
+    batFileTitleBox = TitleBox(app, "start.bat", grid=[1, 1])
     Text(batFileTitleBox, "Enter CMD commands here, separated with a newline")
     batFile = TextBox(batFileTitleBox, text="start steam://run/400 -fullscreen\n"
                                             "exit",multiline=True, width=55, height=15)
 
     # User inputs security code
-    securityCodeBox = TitleBox(driveMaker, "Trust Password", grid=[0, 2])
+    securityCodeBox = TitleBox(app, "Trust Password", grid=[0, 2])
     securityCode = TextBox(securityCodeBox, text="", hide_text=True, width=10, height=1, command=hashIT)
     # Add Icon
-    PushButton(driveMaker, text="Select Icon (.ico)", command=selectIcon, grid=[1,0])
+    PushButton(app, text="Select Icon (.ico)", command=selectIcon, grid=[1, 0])
     # Flash
-    PushButton(driveMaker, text="Flash", command=flashDrive, grid=[1,2])
+    PushButton(app, text="Flash", command=flashDrive, grid=[1, 2])
 
 def hashIT():
     global h
@@ -55,12 +56,22 @@ def flashDrive():
     with open("trust.txt", "w") as f:
         f.write(h.hexdigest())
         f.close()
-    driveMaker.info(title="Drive Flash Utility", text="Drive was flashed successfully, to apply changes unplug your drive and plug it back in.")
+    app.info(title="Drive Flash Utility", text="Drive was flashed successfully, to apply changes unplug your drive and plug it back in.")
     os.chdir("C:\\")
-app = App(title="Install & Creation Tool")
-driveMaker = Window(app, title="Drive Flash Utility", visible=False, layout="grid", width=660, height=370)
-PushButton(app, text="Create Drive", command=createDrive)
-
-
-
+def indexNonNTFSVolumes():
+    nonNTFS.clear()
+    nonNTFS.insert(0, "Select Drive")
+    drives = win32api.GetLogicalDriveStrings()
+    dx = [x for x in drives.split("\000") if x]
+    for drive in dx:
+        try:
+            vol_info = win32api.GetVolumeInformation(drive)
+            if vol_info[4] != "NTFS":
+                # Call GetVolumeInformation
+                nonNTFS.append(drive)
+                print(drive, vol_info[0], vol_info[4])
+        except:
+            print("Drive(s) found:")
+app = App(title="Drive Flash Utility", layout="grid", width=700, height=400)
+createDrive()
 app.display()
